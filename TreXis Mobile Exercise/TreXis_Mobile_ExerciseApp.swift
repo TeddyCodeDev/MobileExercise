d2ae1e6c880b1bd2045e8737b9184835cf49a04f
Tyler Barnes <tylerb@gurutechnologies.net>
2021-08-03 20:32:59 -0600

    Initial Commit

---
+++ TreXis Mobile Exercise/TreXis_Mobile_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreXis_Mobile_ExerciseApp.swift
+//  TreXis Mobile Exercise
+//
+//  Created by Tyler Barnes on 8/3/21.
+//
+
+import SwiftUI
+
+@main
+struct TreXis_Mobile_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
